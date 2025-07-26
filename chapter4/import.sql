9331f9ac9ce332529831685aba157e954413f462
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2025-07-26 21:58:10 +0000

    docs: copy chapter 3 files to chapter 4

---
+++ chapter4/import.sql
@@ -0,0 +1,5 @@
+.import --skip 1 data/player_data.csv player
+.import --skip 1 data/performance_data.csv performance
+.import --skip 1 data/league_data.csv league
+.import --skip 1 data/team_data.csv team
+.import --skip 1 data/team_player_data.csv team_player
